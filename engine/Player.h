592a9c0831049341e00d711f9540f75a2efefd33
Fisab <vladsudakov5@gmail.com>
2016-11-30 05:56:24 +0300

    Update Player.h

--- engine/Player.h
+++ engine/Player.h
@@ -2,26 +2,51 @@
 #define PLAYER_H
 
 #include <SFML/Graphics.hpp>
+#include <vector>
+#include <iostream>
 
 class Player
 {
 public:
 
-	Player(sf::Vector2f);
-	void process();
+	Player(sf::Vector2f pos_);
+	void process(sf::RenderWindow& window, float time, sf::Event *event);
 
 	sf::Vector2f pos;
 	sf::Vector2f vel;
-	void draw(sf::RenderWindow&);
-
-	sf::CircleShape shape;
+	void draw(sf::RenderWindow &window);
 
 private:
-	void gravity();
-	void move();
-	bool checkCollision();
+	const int startAngle = 90;
+	float angle = 0;
+	float curSpeed;
 
-	const int speed = 10;
+	void move(float time);
+	//bool checkCollision();
+
+	const float rotateSpeed = 0.03;
+	const float speed = 2;
+	const int speedLimit = 2;
+	const float slip = 0.9;
+
+	void makeTrail();
+	float intervalTrail = 0;
+
+	sf::Texture playerTexture;
+	sf::Sprite playerSprite;
+
+	sf::Texture trailTexture;
+	sf::Sprite trailSprite;
+
+	struct trail
+	{
+		sf::Sprite spriteTrail;
+		int opacity = 255;
+	};
+
+	sf::Vector2u trailSize;
+
+	std::vector<trail> trails;
 
 };
 
