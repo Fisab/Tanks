c25d9135a3bd3b0811faa00da0d1ec06a82f7963
Fisab <vladsudakov5@gmail.com>
2016-12-02 03:10:14 +0300

    Update Player.cpp

--- engine/Player.cpp
+++ engine/Player.cpp
@@ -9,7 +9,7 @@ Player::Player(sf::Vector2f pos_)
 	playerSprite.setPosition(pos);
 	playerSprite.setScale(0.5, 0.5);
 	playerSprite.setOrigin(playerTexture.getSize().x / 2, playerTexture.getSize().y / 2);
-	playerSprite.setRotation(startAngle);
+	playerSprite.setRotation(radToDeg(startAngle));
 
 	towerTexture.loadFromFile("data/images/Tanks/barrelGreen_outline.png");
 	towerTexture.setSmooth(true);
@@ -17,7 +17,7 @@ Player::Player(sf::Vector2f pos_)
 	towerSprite.setOrigin(towerTexture.getSize().x / 2, towerTexture.getSize().y);
 	towerSprite.setScale(0.5, 0.5);
 	towerSprite.setPosition(pos);
-	towerSprite.setRotation(startAngle);
+	towerSprite.setRotation(radToDeg(startAngle));
 
 	trailTexture.loadFromFile("data/images/Tanks/tracksLarge.png");
 	trailTexture.setSmooth(true);
@@ -28,16 +28,20 @@ Player::Player(sf::Vector2f pos_)
 	trailSprite.setTexture(trailTexture);
 	trailSprite.setTextureRect(sf::IntRect(0, 0, trailSize.x, 30));
 	trailSprite.setOrigin(trailTexture.getSize().x / 2, trailTexture.getSize().y / 2 - 15);
-	trailSprite.setRotation(startAngle);
+	trailSprite.setRotation(radToDeg(startAngle));
 	trailSprite.setColor(sf::Color(169, 169, 169, 255));
 }
 
-void Player::process(sf::RenderWindow& window, float time, sf::Event *event) {
-	if (towerAngle > 360) towerAngle = 0;
-	else if (towerAngle < 0) towerAngle = 360;
+long double Player::radToDeg(long double a) {
+	return a * 180 / M_PI;
+}
 
-	if (angle > 360) angle = 0;
-	else if (angle < 0) angle = 360;
+void Player::process(sf::RenderWindow& window, float time, sf::Event *event) {
+	if (towerAngle > M_PI * 2) towerAngle = 0;
+	else if (towerAngle < 0) towerAngle = M_PI * 2;
+
+	if (angle > M_PI * 2) angle = 0;
+	else if (angle < 0) angle = M_PI * 2;
 
 	rotateTower(window.mapPixelToCoords(sf::Mouse::getPosition(window)), time);
 	Player::move(time);
@@ -59,56 +63,55 @@ void Player::process(sf::RenderWindow& window, float time, sf::Event *event) {
 void Player::move(float time) {
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
 		angle -= rotateSpeed * time;
-		playerSprite.setRotation(angle + startAngle);
-		//towerSprite.setRotation(angle + startAngle + towerAngle);
-		//towerAngle -= rotateSpeed * time;
+		playerSprite.setRotation(radToDeg(angle + startAngle));
+		towerSprite.setRotation(radToDeg(angle + startAngle + towerAngle));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
 		angle += rotateSpeed * time;
-		playerSprite.setRotation(angle + startAngle);
-		//towerSprite.setRotation(angle + startAngle + towerAngle);
-		//towerAngle += rotateSpeed * time;
+		playerSprite.setRotation(radToDeg(angle + startAngle));
+		towerSprite.setRotation(radToDeg(angle + startAngle + towerAngle));
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::W)) {
 		curSpeed = sqrt(vel.x*vel.x + vel.y*vel.y);
 		if (curSpeed < speedLimit) {
-			vel.x += speed * cos(angle / 180 * 3.14);
-			vel.y += speed * sin(angle / 180 * 3.14);
+			vel.x += speed * cos(angle);
+			vel.y += speed * sin(angle);
 		}
 	}
 	if (sf::Keyboard::isKeyPressed(sf::Keyboard::S)) {
 		curSpeed = sqrt(vel.x*vel.x + vel.y*vel.y);
 		if (curSpeed < speedLimit / 2) {
-			vel.x -= speed / 3 * cos(angle / 180 * 3.14);
-			vel.y -= speed / 3 * sin(angle / 180 * 3.14);
+			vel.x -= speed / 3 * cos(angle);
+			vel.y -= speed / 3 * sin(angle);
 		}
 	}
 }
 
 void Player::rotateTower(sf::Vector2f posMouse, float time) {
 	//std::cout << angle << std::endl;
-	double a1 = atan2(pos.y, pos.x);// -angle;
-	double a2 = atan2(posMouse.y - pos.y, posMouse.x - pos.x);
 
-	double a3 = a2 * 180 / 3.14 - towerAngle;
+	long double a1 = atan2(pos.y, pos.x);// -angle;
+	long double a2 = atan2(posMouse.y - pos.y, posMouse.x - pos.x);
 
-	if (a3 > 180) a3 -= 360;
-	else if (a3 < -180) a3 += 360;
+	long double a3 = a2 - towerAngle;
+
+	if (a3 > M_PI / 2) a3 -= M_PI * 2;
+	else if (a3 < -1 * M_PI) a3 += M_PI * 2;
 
 	int dir = 0;
-	if (a3 - a1 > -1 && a3 - a1 < 1) return;
+	if (a3 - a1 > -M_PI/1000 && a3 - a1 < M_PI / 1000) return;
 	if (a3 - a1 < 0) dir = -1;
 	if (a3 - a1 > 0) dir = 1;
 
 	towerAngle += rotateTowerSpeed * time * dir;
 
-	towerSprite.setRotation(startAngle + towerAngle);
-	//towerSprite.setRotation(angle + startAngle + towerAngle);
+	//towerSprite.setRotation(radToDeg(startAngle + towerAngle));
+	towerSprite.setRotation(radToDeg(angle + startAngle + towerAngle));
 }
 
 void Player::makeTrail(){
 	trailSprite.setPosition(pos);
-	trailSprite.setRotation(angle + startAngle);
+	trailSprite.setRotation(radToDeg(angle + startAngle));
 	trails.push_back(trail());
 	trails[trails.size() - 1].spriteTrail = trailSprite;
 
