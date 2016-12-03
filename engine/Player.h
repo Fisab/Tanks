5c90f7af12cda763345cc294196e5ffd06b72e5b
Fisab <vladsudakov5@gmail.com>
2016-12-04 00:56:05 +0300

    Update Player.h

--- engine/Player.h
+++ engine/Player.h
@@ -6,6 +6,7 @@
 #include <iostream>
 #include <math.h>
 #include <algorithm>
+#include "Bullet.h"
 
 # define M_PI           3.141592653589793
 
@@ -27,6 +28,7 @@ private:
 	float curSpeed;
 
 	void move(float time);
+	void shoot(float time);
 	//bool checkCollision();
 
 	long double radToDeg(long double a);
@@ -34,11 +36,13 @@ private:
 	long double getMin(long double a, long double b);
 	int getSign(int a);
 
-	const float rotateSpeed = M_PI / 6500;
-	const float rotateTowerSpeed = M_PI / 60;
+	const float rotateSpeed = M_PI / 5000;
+	const float rotateTowerSpeed = M_PI / 3500;
 	const float speed = 2;
 	const int speedLimit = 2;
 	const float slip = 0.9;
+
+	void bulletsProcess(sf::RenderWindow& window, float time);
 
 	void makeTrail();
 	float intervalTrail = 0;
@@ -60,10 +64,25 @@ private:
 		int opacity = 255;
 	};
 
+	sf::Texture smokeTexture0;
+	sf::Texture smokeTexture1;
+	sf::Texture smokeTexture2;
+	sf::Sprite smokeSprite;
+	int shootTick = 0;
+
+	void processSmoke();
+
+	struct smoke {
+		sf::Sprite sprite;
+		int opacity = 255;
+		double scale = 0.1;
+	};
+
 	sf::Vector2u trailSize;
 
 	std::vector<trail> trails;
-
+	std::vector<smoke> smokes;
+	std::vector<Bullet*> bullets;
 };
 
 #endif
